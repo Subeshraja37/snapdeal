871082dd681007beeb579a67d204d701cfc6a2ba
Subeshraja37 <subeshraja37@gmail.com>
2023-01-21 12:23:26 +0530

    First time

---
+++ src/test/java/org/base/Simple.java
@@ -0,0 +1,17 @@
+package org.base;
+
+public class Simple {
+private void method1() {
+	System.out.println("first method");
+
+}
+private void method2() {
+System.out.println("second method");
+}
+private void method3() {
+ System.out.println("third method");
+}
+private void method4() {
+System.out.println("fourth method");
+}
+}
